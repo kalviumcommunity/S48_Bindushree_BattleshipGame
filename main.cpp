935e86777943d836c4f4937d6561c6ceb58f2aeb
bindhushree <bindhushreeb05@gmail.com>
2024-08-27 22:31:16 +0530

    done

--- main.cpp
+++ main.cpp
@@ -3,23 +3,14 @@
 #include <cstdlib>
 #include <ctime>
 
-// Class to represent a Ship
 class Ship {
-private:
+public:
     int x, y;
     bool sunk;
 
-public:
     Ship(int x, int y) : x(x), y(y), sunk(false) {}
-
-    int getX() const { return this->x; }
-    int getY() const { return this->y; }
-    bool isSunk() const { return this->sunk; }
-
-    void sink() { this->sunk = true; }
 };
 
-// Class to represent the Game Board
 class Board {
 private:
     std::vector<std::vector<char>> grid;
@@ -27,50 +18,54 @@ private:
     int size;
 
 public:
-    Board(int size) : size(size) {
-        this->grid.resize(size, std::vector<char>(size, '-'));
-    }
+    Board(int size) : size(size), grid(size, std::vector<char>(size, '-')) {}
 
     void placeShip(int x, int y) {
-        this->ships.emplace_back(x, y);
-        this->grid[x][y] = 'S';
+        ships.emplace_back(x, y);
+        grid[x][y] = 'S';
     }
 
     bool attack(int x, int y) {
-        if (this->grid[x][y] == 'S') {
-            this->grid[x][y] = 'X';
-            for (auto& ship : this->ships) {
-                if (ship.getX() == x && ship.getY() == y) {
-                    ship.sink();
-                    std::cout << "Hit! Ship at (" << x << ", " << y << ") has been sunk!\n";
-                    return true;
-                }
+        if (x < 0 || x >= size || y < 0 || y >= size) {
+            std::cout << "Out of bounds!\n";
+            return false;
+        }
+
+        if (grid[x][y] == 'S') {
+            grid[x][y] = 'X';
+            std::cout << "Hit at (" << x << ", " << y << ")!\n";
+            for (auto& ship : ships) {
+                if (ship.x == x && ship.y == y) ship.sunk = true;
             }
+            return true;
+        }
+
+        if (grid[x][y] == 'X' || grid[x][y] == 'O') {
+            std::cout << "Already attacked (" << x << ", " << y << ").\n";
         } else {
-            this->grid[x][y] = 'O';
-            std::cout << "Miss at (" << x << ", " << y << ")\n";
+            grid[x][y] = 'O';
+            std::cout << "Miss at (" << x << ", " << y << ").\n";
         }
         return false;
     }
 
     void printBoard() const {
-        for (int i = 0; i < this->size; ++i) {
-            for (int j = 0; j < this->size; ++j) {
-                std::cout << this->grid[i][j] << " ";
+        for (const auto& row : grid) {
+            for (char cell : row) {
+                std::cout << cell << ' ';
             }
-            std::cout << std::endl;
+            std::cout << '\n';
         }
     }
 
     bool allShipsSunk() const {
-        for (const auto& ship : this->ships) {
-            if (!ship.isSunk()) return false;
+        for (const auto& ship : ships) {
+            if (!ship.sunk) return false;
         }
         return true;
     }
 };
 
-// Function to play the game
 void playGame(Board& board) {
     int x, y;
     while (!board.allShipsSunk()) {
@@ -79,21 +74,21 @@ void playGame(Board& board) {
         std::cin >> x >> y;
         board.attack(x, y);
     }
-    std::cout << "Congratulations! You've sunk all the ships!\n";
+    std::cout << "All ships sunk! You win!\n";
 }
 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
 
-    const int boardSize = 5;
-    const int numShips = 3;
+    Board board(5);
+    int numShips = 3;
 
-    Board board(boardSize);
-
-    // Randomly place ships
     for (int i = 0; i < numShips; ++i) {
-        int x = rand() % boardSize;
-        int y = rand() % boardSize;
+        int x, y;
+        do {
+            x = rand() % 5;
+            y = rand() % 5;
+        } while (board.attack(x, y)); // Use attack to check if the cell is already occupied
         board.placeShip(x, y);
     }
 
